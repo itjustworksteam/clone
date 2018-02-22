bc9b681d495fb2ff2e9dbcd1df0d153d4d04b974
Riccardo Crippa <therickys93@gmail.com>
2018-02-22 20:30:14 +0100

    add github org clone

---
+++ fromgithuborg.py
@@ -0,0 +1,10 @@
+import json
+import os
+
+orgs = raw_input("Enter organization from github: ")
+os.system("curl -k https://api.github.com/orgs/"+orgs+"/repos > repoinfo")
+json_data = open("./repoinfo").read()
+data = json.loads(json_data)
+for repo in data:
+	os.system("git clone "+repo["clone_url"])
+os.system("rm repoinfo")
