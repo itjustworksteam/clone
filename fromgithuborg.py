f9c20b73d7e62f6490f71c1aadb5bc19c9b80b06
Riccardo Crippa <therickys93@gmail.com>
2018-02-22 21:20:45 +0100

    remove limit

--- fromgithuborg.py
+++ fromgithuborg.py
@@ -2,7 +2,7 @@ import json
 import os
 
 orgs = raw_input("Enter organization from github: ")
-os.system("curl -k https://api.github.com/orgs/"+orgs+"/repos > repoinfo")
+os.system("curl -k https://api.github.com/orgs/"+orgs+"/repos?per_page=200 > repoinfo")
 json_data = open("./repoinfo").read()
 data = json.loads(json_data)
 for repo in data:
